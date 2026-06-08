c8fcba9cdbf3ccda84df17593a03ddd1c14dd87c
Cath <cathleen@outlook.dk>
2026-06-08 17:24:16 +0200

    header and footer all done plus burger and extra things related to that

---
+++ burger.js
@@ -0,0 +1,6 @@
+const hamMenu = document.querySelector(".ham_menu");
+const offScreenMenu = document.querySelector(".off_screen_menu");
+hamMenu.addEventListener("click", () => {
+  hamMenu.classList.toggle("active");
+  offScreenMenu.classList.toggle("active");
+});
