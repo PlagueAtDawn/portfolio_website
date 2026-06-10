f93e16d08105413d687319e14cb9935864ce95fd
Cath <cathleen@outlook.dk>
2026-06-10 07:41:34 +0200

    i did more than i wanted

--- projects.js
+++ projects.js
@@ -1,2 +1,16 @@
-document.body.style.overflow = "hidden";
-document.body.style.overflow = "";
+const popovers = document.querySelectorAll("[popover]");
+
+popovers.forEach((popover) => {
+  const closeButton = popover.querySelector("button");
+
+  closeButton.addEventListener("click", (event) => {
+    event.preventDefault();
+
+    popover.classList.add("closing");
+
+    setTimeout(() => {
+      popover.hidePopover();
+      popover.classList.remove("closing");
+    }, 300);
+  });
+});
